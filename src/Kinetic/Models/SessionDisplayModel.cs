4f2fae192647026f2742b0fe8f2e92a0ed894403
rmccabe24 <robert.mccabe@outlook.com>
2023-05-28 20:46:47 +0100

    Update

---
+++ src/Kinetic/Models/SessionDisplayModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Kinetic.Presentation.Models
+{
+    class SessionDisplayModel
+    {
+        public string TotalTime { get; set; }
+        public string Session { get; set; }
+        public string Distance { get; set; }
+        public bool Complete { get; set; }
+        public string TextColor { get; set; }
+    }
+}
